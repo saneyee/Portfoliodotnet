69a26d83df6acd42af7c6e2e8435a5a191eb4d44
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-21 21:17:00 -0800

    login works

--- Portfolio/Controllers/AccountController.cs
+++ Portfolio/Controllers/AccountController.cs
@@ -52,6 +52,26 @@ namespace Portfolio.Controllers
         }
 
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
 
     }
 }
