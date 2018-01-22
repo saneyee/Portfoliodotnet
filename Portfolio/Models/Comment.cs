07e9197b383c6453facd26e90c95377c5bedba02
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-21 23:39:09 -0800

    adds model post and model comment

---
+++ Portfolio/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Portfolio.Models
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public int PostId { get; set; }
+        public virtual Post Posts { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
