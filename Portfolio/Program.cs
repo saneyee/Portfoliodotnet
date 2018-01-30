6c2a4ddf4ff00bb559726e54bc7d4b0a9517b87a
Saneyee Sarkar <saneyeesarkar@gmail.com>
2018-01-29 20:32:57 -0800

    adds the restsharp.netcore package

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -9,16 +9,7 @@ namespace Portfolio
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
-        }
+        
+    
     }
 }
